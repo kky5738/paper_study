df12557025cb4d72823a826407675cc8e894451a
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2024-02-08 17:40:42 +0900

    Update transformer_from_wikidocs.py
    - Transformer 전체 구조 정의 완료
    - 학습 관련 코드는 업데이트 예정

--- Transformer/transformer_from_wikidocs.py
+++ Transformer/transformer_from_wikidocs.py
@@ -25,17 +25,50 @@ class Transformer(nn.Module):
         """
         super().__init__()
         
+        # Model Info
+        self.model_type = "Transformer"
+        self.dim_model = dim_model
+        
         # Entire Layers
-        self.transformer = nn.Transformer(
+        self.positional_encoder = PositionalEncoding(dim_model=dim_model, dropout_rate=dropout_rate, max_len=5000)
+        self.embedding = nn.Embedding(num_tokens, dim_model)
+        self.transformer = nn.Transformer( # 여기도 직접 구현하면 더 좋을 것 같은데..
             d_model=dim_model,
             nhead=num_heads,
             num_encoder_layers=num_encoder_layers,
             num_decoder_layers=num_decoder_layers,
             dropout=dropout_rate,
         )
+        self.out = nn.Linear(dim_model, num_tokens)
     
-    def forward(self):
-        pass
+    def forward(self, src, tgt, tgt_mask=None, src_pad_mask=None, tgt_pad_mask=None):
+        """
+        왜 positional encoding 하기 전에 embedding 값에 sqrt(dim_model)을 곱하나요?
+        """
+        src = self.embedding(src) * math.sqrt(self.dim_model)
+        tgt = self.embedding(tgt) * math.sqrt(self.dim_model)
+        
+        src = self.positional_encoder(src)
+        tgt = self.positional_encoder(tgt)
+        
+        src = src.permute(1,0,2)
+        tgt = tgt.permute(1,0,2)
+        
+        transformer_out = self.transformer(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=src_pad_mask, tgt_key_padding_mask=tgt_pad_mask)
+        out = self.out(transformer_out)
+        
+        return out
+    
+    def get_tgt_mask(self, size) -> torch.tensor:
+        mask = torch.tril(torch.ones(size, size) == 1) # Lower trianular matrix : 하삼각행렬??
+        mask = mask.float()
+        mask = mask.masked_fill(mask == 0, float('-inf'))
+        mask = mask.masked_fill(mask == 1, float(0.0))
+        
+        return mask
+    
+    def create_pad_mask(self, matrix: torch.tensor, pad_token: int) -> torch.tensor:
+        return (matrix == pad_token)
     
 class PositionalEncoding(nn.Module):
     def __init__(self, dim_model, dropout_rate, max_len):
@@ -55,4 +88,4 @@ class PositionalEncoding(nn.Module):
         
     def forward(self, token_embedding: torch.tensor) -> torch.tensor:
         return self.dropout(token_embedding + self.pos_encoding[:token_embedding.size(0), :])
-        
+
